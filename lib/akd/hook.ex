b55ec507031940e8a947aceaadc62d5751bfc411
Adi Iyengar <aditya7iyengar@gmail.com>
2017-12-11 19:24:20 -0500

    Add better exec_dest to akd

--- lib/akd/hook.ex
+++ lib/akd/hook.ex
@@ -6,23 +6,25 @@ defmodule Akd.Hook do
   alias Akd.{Destination, Deployment, Hook, SecureConnection}
 
   @enforce_keys ~w(commands exec_dest)a
-  @optional_keys ~w(opts)a
+  @optional_keys ~w(cleanup opts)a
 
   defstruct @enforce_keys ++ @optional_keys
 
   @typedoc ~s(Generic type for a Hook with all enforced keys)
   @type t :: %__MODULE__{
     commands: String.t | :noop,
-    exec_dest: Akd.Destination.t,
+    exec_dest: Akd.Destination.t | :build_env | :publish_env | :local,
+    cleanup: String.t | :noop,
     opts: list | nil
   }
 
   @callback commands(deployment :: Deployment.t, opts :: list) :: String.t
+  @callback cleanup(deployment :: Deployment.t, opts :: list) :: String.t
 
   @doc ~s()
   @spec exec(Hook.t) :: {:ok, String.t} | {:error, String.t}
   def exec(hook)
-  def exec(%Hook{exec_dest: %Destination{sshserver: :local}} = hook) do
+  def exec(%Hook{exec_dest: :local} = hook) do
     with {output, 0} <- System.cmd("sh", ["-c" , hook.commands], cd: hook.exec_dest.path) do
       {:ok, output}
     else
