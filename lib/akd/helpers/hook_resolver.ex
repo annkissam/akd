06a7bc42e2a921ffa76691a2c7fd5b79024cfcb0
Adi Iyengar <aditya7iyengar@gmail.com>
2017-12-11 21:49:07 -0500

    Add hooks resolver module

---
+++ lib/akd/helpers/hook_resolver.ex
@@ -0,0 +1,29 @@
+defmodule Akd.HookResolver do
+  @moduledoc """
+  """
+
+  alias Akd.Config
+
+  @stages ~w(fetch init build publish)a
+  for stage <- @stages do
+    noun = stage
+      |> (&(to_string(&1) <> "er")).()
+
+    modname = noun
+      |> Macro.camelize()
+      |> (& "Elixir.Akd." <> &1).()
+
+    def unquote(stage)(deployment, []) do
+      unquote(noun)
+      |> String.to_atom()
+      |> (&apply(Config, &1, [])).()
+      |> apply(:get_hook, [deployment, []])
+    end
+
+    def unquote(stage)(deployment, opts) do
+      type = opts[:type] |> to_string() |> Macro.camelize()
+      mod = String.to_existing_atom(unquote(modname) <> "." <> type)
+      apply(mod, :get_hook, [deployment, opts])
+    end
+  end
+end
