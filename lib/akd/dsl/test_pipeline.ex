2479f0969f4fd768e960d8eaf4c2259e6c4916a8
Adi Iyengar <aditya7iyengar@gmail.com>
2017-12-11 22:47:30 -0500

    Update test pipeline accordinly

--- lib/akd/dsl/test_pipeline.ex
+++ lib/akd/dsl/test_pipeline.ex
@@ -8,8 +8,8 @@ defmodule TestPipeline do
   pipeline :build do
     hook {:base, :build}
     hook {:build_env, commands: "MIX_ENV=prod mix deploy", cleanup: "rm -rf _build"}
-    hook %Akd.Hook{commands: "MIX_ENV=prod mix release --env=prod", runat: %Akd.Destination{sshuser: "annadmin", sshserver: "10.11.5.77", path: "~/elixir_apps/"}}
-    hook %Akd.Hook{commands: "MIX_ENV=prod mix release --env=prod", runat: %Akd.Destination{sshuser: "annadmin", sshserver: "10.11.5.77", path: "~/elixir_apps/"}}
+    hook %Akd.Hook{commands: "MIX_ENV=prod mix release --env=prod", runat: %Akd.Destination{user: "annadmin", server: "10.11.5.77", path: "~/elixir_apps/"}}
+    hook %Akd.Hook{commands: "MIX_ENV=prod mix release --env=prod", runat: %Akd.Destination{user: "annadmin", server: "10.11.5.77", path: "~/elixir_apps/"}}
   end
 
   pipeline :deploy do
