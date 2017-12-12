29053ce851a71675fb42bfb60fe18624c994ef2f
Adi Iyengar <aditya7iyengar@gmail.com>
2017-12-11 21:48:52 -0500

    Add params helper for sample deploy

---
+++ lib/akd/mix/sample_deploy/params_helper.ex
@@ -0,0 +1,13 @@
+defmodule Akd.Mix.SampleDeploy.ParamsHelper do
+  def translated_params(parsed_params) do
+    parsed_params
+    |> sort_keys()
+    |> translate_params()
+  end
+
+  defp sort_keys(params), do: Enum.sort_by(params, &elem(&1, 0))
+
+  defp translate_params([appname: a, buildat: b, env: e, publishto: p]) do
+    %{env: e, buildat: b, publishto: p, appname: a, version: Mix.Project.config[:version]}
+  end
+end
