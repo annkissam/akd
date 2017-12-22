ec05e135c5adfbc8723e20a8379ca2776b5f46b8
Adi <aditya7iyengar@gmail.com>
2017-12-22 01:27:28 -0500

    Update deploy helper:
    - Add ability to use Hook module
    - Add ability to pass opts to Hook module

--- lib/akd/helpers/deploy_helper.ex
+++ lib/akd/helpers/deploy_helper.ex
@@ -6,7 +6,7 @@ defmodule Akd.DeployHelper do
 
   alias Akd.{Destination, DestinationResolver, Deployment, Hook, HookResolver}
 
-  @base_types ~w(fetch init build stopapp publish startapp)a
+  @base_types ~w(fetch init build stopnode publish startnode)a
 
   defmacro execute(pipeline, with: block) do
     quote do
@@ -20,18 +20,11 @@ defmodule Akd.DeployHelper do
   end
 
   def exec(%Deployment{hooks: hooks}) do
-    {stop, called_hooks} = Enum.reduce(hooks, {false, []}, fn(hook, {stop, called_hooks}) ->
-      with false <- stop,
-        {:ok, output} <- Hook.exec(hook)
-      do
-        {stop, [hook | called_hooks]}
-      else
-        {:error, error} -> {true, called_hooks}
-        true -> {true, called_hooks}
-      end
-    end)
+    {stop, called_hooks} = Enum.reduce(hooks, {false, []}, &stop_and_hooks/2)
 
+    # Add rollback here
     Enum.each(called_hooks, &Hook.cleanup/1)
+    if stop, do: Enum.each(called_hooks, &Hook.rollback/1)
   end
 
   def init_deployment(params) do
@@ -46,10 +39,13 @@ defmodule Akd.DeployHelper do
   def add_hook(%Deployment{hooks: hooks} = deployment, %Hook{} = hook) do
     %Deployment{deployment | hooks: hooks ++ [hook]}
   end
-  def add_hook(deployment, {:base, type}) when type in @base_types do
-    add_hook(deployment, get_hook(deployment, type, []))
+  def add_hook(%Deployment{hooks: hooks} = deployment, {%Hook{} = hook, _}) do
+    %Deployment{deployment | hooks: hooks ++ [hook]}
   end
-  def add_hook(deployment, {:base, type, opts}) when type in @base_types do
+  def add_hook(deployment, {mod, opts}) when is_atom(mod) do
+    add_hook(deployment, get_hook(deployment, mod, opts))
+  end
+  def add_hook(deployment, {type, opts}) when type in @base_types do
     add_hook(deployment, get_hook(deployment, type, opts))
   end
   def add_hook(deployment, opts) do
@@ -65,11 +61,28 @@ defmodule Akd.DeployHelper do
       %Hook{commands: commands, runat: runat, cleanup: cleanup, env: env})
   end
 
-  defp get_hook(d, type, opts), do: apply(HookResolver, type, [d, opts])
+  defp stop_and_hooks(hook, {stop, called_hooks}) do
+    with false <- stop,
+      {:ok, _output} <- Hook.exec(hook)
+    do
+      {stop, [hook | called_hooks]}
+    else
+      {:error, _err} ->
+        hook.ignore_failure && {false, called_hooks} || {true, called_hooks}
+      true -> {true, called_hooks}
+    end
+  end
+
+  defp get_hook(d, type, opts) when type in @base_types do
+    apply(HookResolver, type, [d, opts])
+  end
+  defp get_hook(d, mod, opts), do: apply(mod, :get_hook, [d, opts])
 
   defp sanitize(%Deployment{buildat: b, publishto: p} = deployment) do
     %Deployment{deployment | buildat: to_dest(b), publishto: to_dest(p)}
   end
 
-  defp to_dest(d), do: is_binary(d) && Destination.parse(d) || d
+  defp to_dest({:local, path}), do: Destination.local(path)
+  defp to_dest(d) when is_binary(d), do: Destination.parse(d)
+  defp to_dest(%Destination{} = d), do: d
 end
