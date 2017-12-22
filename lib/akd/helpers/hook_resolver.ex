6a24df6507b75205b2c0fce34d51bb03279a6c10
Adi <aditya7iyengar@gmail.com>
2017-12-22 01:26:53 -0500

    Update hook resolver to work with new task

--- lib/akd/helpers/hook_resolver.ex
+++ lib/akd/helpers/hook_resolver.ex
@@ -2,17 +2,16 @@ defmodule Akd.HookResolver do
   @moduledoc """
   """
 
-  alias Akd.{Config, Deployment, DestinationResolver, Hook}
+  alias Akd.{Config, DestinationResolver, Hook}
 
   @stages ~w(fetch init build publish)a
 
   for stage <- @stages do
-    noun = stage
-      |> (&(to_string(&1) <> "er")).()
+    noun = to_string(stage) <> "er"
 
-    modname = noun
+    binding = noun
       |> Macro.camelize()
-      |> (& "Elixir.Akd." <> &1).()
+      |> (&Module.concat(Akd, &1)).()
 
     def unquote(stage)(deployment, []) do
       unquote(noun)
@@ -22,22 +21,25 @@ defmodule Akd.HookResolver do
     end
 
     def unquote(stage)(deployment, opts) do
-      type = opts[:type] |> to_string() |> Macro.camelize()
-      mod = String.to_existing_atom(unquote(modname) <> "." <> type)
+      mod = opts[:type]
+        |> to_string()
+        |> Macro.camelize()
+        |> (&Module.concat(unquote(binding), &1)).()
+
       apply(mod, :get_hook, [deployment, opts])
     end
   end
 
-  for hook <- [:stop, :start] do
+  for hook <- ~w(start stop)a do
     method_name = hook
-      |> (&Atom.to_string(&1) <> "app").()
+      |> (&Atom.to_string(&1) <> "node").()
       |> String.to_atom()
 
-    def unquote(method_name)(%Deployment{appname: appname} = deployment, opts) do
+    def unquote(method_name)(deployment, opts) do
       runat = opts[:runat] || DestinationResolver.resolve(:publish, deployment)
 
-      %Hook{commands: "bin/#{appname} #{unquote(hook)}",
-        runat: runat, env: opts[:env]}
+      %Hook{commands: "bin/#{deployment.appname} #{unquote(hook)}",
+        runat: runat, env: opts[:env], ignore_failure: opts[:ignore_failure]}
     end
   end
 end
