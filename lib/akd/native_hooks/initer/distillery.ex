06553875d8ac0f4e0a4fec7376c1ca9a9adc7896
Adi <aditya7iyengar@gmail.com>
2017-12-22 01:30:22 -0500

    Update distillery initer

--- lib/akd/native_hooks/initer/distillery.ex
+++ lib/akd/native_hooks/initer/distillery.ex
@@ -4,17 +4,34 @@ defmodule Akd.Initer.Distillery do
   with specific parameters.
   """
 
-  @behaviour Akd.Hook
+  use Akd.Hook
 
-  alias Akd.{Deployment, Destination, DestinationResolver, Hook}
+  alias Akd.{Deployment, DestinationResolver, Hook}
 
   def get_hook(%Deployment{appname: appname} = deployment, opts) do
-    commands = """
-      mix release.init --no-doc --name=#{appname}"
-    """
-
     runat = opts[:runat] || DestinationResolver.resolve(:build, deployment)
+    env = deployment.env
 
-    %Hook{commands: commands, runat: runat, env: opts[:env]}
+    %Hook{commands: commands(appname, env), runat: runat, env: opts[:env]}
+  end
+
+  defp commands(nil, env) do
+    """
+    #{setup(env)}
+    MIX_ENV=#{env} mix release.init --no-doc
+    """
+  end
+  defp commands(appname, env) do
+    """
+    #{setup(env)}
+    MIX_ENV=#{env} mix release.init --no-doc --name #{appname}
+    """
+  end
+
+  defp setup(env) do
+    """
+    MIX_ENV=#{env} mix deps.get
+    MIX_ENV=#{env} mix compile
+    """
   end
 end
