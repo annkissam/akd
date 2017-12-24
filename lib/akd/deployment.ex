bc73552b005a255f806c7346fc808f1fa4127145
Adi <aditya7iyengar@gmail.com>
2017-12-24 00:35:14 -0500

    Update Akd.Deployment:
    - Add module docs to support the module.
    - Update struct with key names that make more sense.
    - Update type t with proper inner types.

--- lib/akd/deployment.ex
+++ lib/akd/deployment.ex
@@ -1,25 +1,36 @@
 defmodule Akd.Deployment do
   @moduledoc """
-  This module encapsulates a `Deployment` struct which contains information
-  such as environment, build environment, publish environment, name of the app,
-  app's version and deployable path.
+  This module represents a `Deployment` struct which contains metadata about
+  a deployment.
+
+  The meta data involves:
+
+  * build_at - `Akd.Destination.t` where the app/node will be built/released.
+  * mix_env - Mix environment to build the app, represented by a `String.t`.
+  * name - Name with which the app/node will be published.
+  * publish_to - `Akd.Destination.t` where to app/node will be published.
+  * vsn - Version of app that is being released, represented by a `String.t`.
+  * hooks - A list of `Akd.Hook.t` that will be run in order when a deployment
+            is executed.
+
+  This struct is mainly used by native hooks in `Akd`, but it can be leveraged
+  to produce custom hooks.
   """
 
   alias Akd.{Destination, Hook}
 
-  # TODO default hooks to []
-  @enforce_keys ~w(env buildat publishto appname version)a
-  @optional_keys ~w(hooks)a
+  @enforce_keys ~w(mix_env build_at publish_to name vsn)a
+  @optional_keys [hooks: []]
 
   defstruct @enforce_keys ++ @optional_keys
 
   @typedoc ~s(Generic type for a Deployment struct)
   @type t :: %__MODULE__{
-    env: String.t,
-    buildat: Destination.t,
-    publishto: Destination.t,
-    appname: Atom.t,
-    version: String.t | nil,
-    hooks: [Hook.t] | nil,
+    mix_env: String.t,
+    build_at: Destination.t,
+    publish_to: Destination.t,
+    name: String.t,
+    vsn: String.t,
+    hooks: [Hook.t],
   }
 end
