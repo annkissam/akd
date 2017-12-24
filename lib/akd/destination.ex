49d8a28e94faec61364948e103cfbab73c1a3c8b
Adi <aditya7iyengar@gmail.com>
2017-12-24 00:49:02 -0500

    Update Akd.Desination:
    - Update key names to more sensible names.
    - Add Documentation to the module.
    - Add Documentation to functions.
    - Add Doc Tests to the functions.
    - Use Hindly Milner Type inference.

--- lib/akd/destination.ex
+++ lib/akd/destination.ex
@@ -1,35 +1,113 @@
 defmodule Akd.Destination do
   @moduledoc """
+  This module represents a `Destination` struct which contains metadata about
+  a destination/location/host.
+
+  The meta data involves:
+
+  * user - Represents the user who will be accessing a host/server.
+           Expects a string, defaults to `:current`.
+  * host - Represents the host/server being accessed.
+           Expects a string, defaults to `:local`.
+  * path - Represents the path on the server being accessed.
+           Expects a string, defaults to `.` (current directory).
+
+  Example:
+    Accessing `root@x.x.x.x:/path/to/dir" would have
+    user: `"root"`
+    host: `"x.x.x.x"`
+    path: `"/path/to/dir/"`
+
+  This struct is mainly used by native hooks in `Akd`, but it can be leveraged
+  to produce custom hooks.
   """
 
-  alias Akd.Destination
+  defstruct [user: :current, host: :local, path: "."]
 
-  @enforce_keys [:user, :server, :path]
+  @typedoc ~s(A `Akd.Destination.user` can be either a string or `:current`)
+  @type user :: String.t | :current
 
-  defstruct @enforce_keys
+  @typedoc ~s(A `Akd.Destination.host` can be either a string or `:local`)
+  @type host :: String.t | :local
 
-  @typedoc ~s(Generic type for Destination with all enforced keys)
+  @typedoc ~s(Generic type for Akd.Destination)
   @type t :: %__MODULE__{
-    user: String.t | :current,
-    server: String.t | :local,
+    user: user,
+    host: host,
     path: String.t
   }
 
-  @doc ~s()
-  @spec to_s(Destination.t) :: String.t
+  @doc """
+  Takes an `Akd.Destination.t` struct, `dest` and parses it into a readable string.
+
+  ##  Examples
+  When `dest` is a local destination:
+
+      iex> params = %{user: :current, host: :local, path: "/path/to/dir"}
+      iex> local_destination = struct!(params, Akd.Destination)
+      iex> Akd.Destination.to_string(local_destination)
+      "/path/to/dir"
+
+
+  When `dest` remote destination:
+
+      iex> params = %{user: "dragonborn", host: "skyrim", path: "whiterun"}
+      iex> local_destination = struct!(params, Akd.Destination)
+      iex> Akd.Destination.to_string(local_destination)
+      "dragonborn@skyrim:whiterun"
+
+  """
+  @spec to_s(__MODULE__.t) :: String.t
   def to_s(dest)
-  def to_s(%Destination{user: :current, server: :local, path: path}), do: path
-  def to_s(%Destination{user: user, server: ip, path: path}) do
+  def to_s(%__MODULE__{user: :current, host: :local, path: path}), do: path
+  def to_s(%__MODULE__{user: user, host: ip, path: path}) do
     "#{user}@#{ip}:#{path}"
   end
 
-  @spec parse(String.t) :: Destination.t
+  @doc """
+  Takes a readable string and converts it to an `Akd.Destination.t` struct.
+  Expects the string to be in the following format:
+    `<user>@<host>:<path>`
+  and parses it to:
+    `%Akd.Destination{user: <user>, host: <host>, path: <path>}`
+
+  Raises a `MatchError` if the string isn't in the correct format.
+
+  ## Examples
+  When a string with the correct format is given:
+
+      iex> Akd.Destination.parse("dragonborn@skyrim:whiterun")
+      %Akd.Desination{user: "dragonborn", host: "skyrim", path: "whiterun"}
+
+  When a wrongly formatted string is given:
+
+      iex> Akd.Desination.parse("arrowtotheknee")
+      ** (MatchError) no match of right hand side value: ["arrowtotheknee"]
+
+  """
+  @spec parse(String.t) :: __MODULE__.t
   def parse(string) do
-    [user, server, path] = Regex.split(~r{@|:}, string)
-    %Destination{user: user, server: server, path: path}
+    [user, host, path] = Regex.split(~r{@|:}, string)
+    %__MODULE__{user: user, host: host, path: path}
   end
 
+
+  @doc """
+  Takes a string path and returns a local `Akd.Destination.t` struct which
+  corresponds to locahost with the given `path`.
+
+  __Alternatively one can initialize an `Akd.Destination.t` struct with just
+  a path, which will return a local Destination struct by default__
+
+  ## Examples
+  When a path is given:
+
+      iex> Akd.Destination.parse("/fus/ro/dah")
+      %Akd.Desination{user: :current, host: :local, path: "fus/ro/dah"}
+
+  """
+  @spec local(String.t) :: __MODULE__.t
   def local(path \\ ".") do
-    %Destination{user: :current, server: :local, path: path}
+    %__MODULE__{user: :current, host: :local, path: path}
   end
 end
