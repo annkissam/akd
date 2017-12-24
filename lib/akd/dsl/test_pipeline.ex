9c4b64336788c7e67fc3ddf4791fc24558d6055a
Adi <aditya7iyengar@gmail.com>
2017-12-24 11:09:54 -0500

    Fix test_pipeline

--- lib/akd/dsl/test_pipeline.ex
+++ lib/akd/dsl/test_pipeline.ex
@@ -1,43 +1,19 @@
 defmodule TestPipeline do
   @moduledoc false
 
-  import Akd.Pipeline
+  import Akd.Dsl.Pipeline
 
   pipeline :init do
-    # hook Akd.Initer.Distillery
-    hook do
-      main "mix release --init", :build, cmd_env: [{"MIX_ENV", "prod"}]
-      ensure "rm -rf rel", :build
-      rollback "rm -rf _build/prod", :build
-    end
-  end
-
-  pipeline :publish do
-    hook ignore_failure: true do
-      main "bin/app stop", :publish
-    end
-
-    hook do
-      main "scp _build/prod/path/to/rel root@host:path/to/dir", :build
-      rollback "rm app.tar.gz", :publish
-    end
-
-    hook do
-      main "bin/app start", :publish, cmd_env: [{"DATABASE_URL", "url"}]
-      ensure "rm app.tar.gz", :publish
-      rollback "bin/app stop", :publish
-    end
+    hook Akd.Initer.Distillery
   end
 
   pipeline :build do
-    hook {:base, :build}
-    hook [runat: :build, commands: "MIX_ENV=prod mix deploy", cleanup: "rm -rf _build"]
-    hook %Akd.Hook{commands: "MIX_ENV=prod mix release --env=prod", runat: %Akd.Destination{user: "root", server: "127.0.0.1", path: "~/apps/"}}
+    hook :build
   end
 
   pipeline :deploy do
     hook :stopnode
-    pipe_through :initialize
+    pipe_through :init
     pipe_through :build
     hook :startnode
   end
