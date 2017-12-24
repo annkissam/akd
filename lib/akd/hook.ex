a4ef1d8292902e0ccf18f372f77245dcf281bec5
Adi <aditya7iyengar@gmail.com>
2017-12-24 05:09:37 -0500

    Update Akd.Hook:
    - Add run_ensure key to control cleanup per hook
    - Decrease metaprogramming, it was getting a little hard to understand

--- lib/akd/hook.ex
+++ lib/akd/hook.ex
@@ -9,12 +9,14 @@ defmodule Akd.Hook do
 
   * ensure - A list of `Akd.Operation.t` structs that are ran after a deployment,
             if the hook was successfully executed (independent of whether the
-            deployment itself was successful or not).
+            deployment itself was successful or not), and `run_ensure` is `true`.
   * ignore_failure - If `true`, the deployment continues to happen even if this
                     hook fails. Defauls to `false`.
   * main - A list of `Akd.Operation.t` that are ran when the hook is executed.
   * rollback - A list of `Akd.Operation.t` that are ran when a deployment is a
               failure, but the hook was called.
+  * run_ensure - If `true`, `ensure` commands are ran independent of whether
+                deployment was successful or not. Defaults to `true`.
 
   This struct is mainly used by native hooks in `Akd`, but it can be leveraged
   to write custom hooks.
@@ -23,7 +25,7 @@ defmodule Akd.Hook do
   alias Akd.{Deployment, Operation}
 
   defstruct [ensure: [], ignore_failure: false,
-              main: [], rollback: []]
+              main: [], rollback: [], run_ensure: true]
 
   @typedoc ~s(Generic type for a Hook struct)
   @type t :: %__MODULE__{
@@ -31,6 +33,7 @@ defmodule Akd.Hook do
     ignore_failure: boolean(),
     main: [Operation.t],
     rollback: [Operation.t],
+    run_ensure: boolean()
   }
 
   @callback get_hooks(Deployment.t, list) :: [__MODULE__.t]
@@ -52,27 +55,72 @@ defmodule Akd.Hook do
   end
 
 
-  for ops_type <- ~w(ensure main rollback)a do
+  @doc """
+  Takes a `Akd.Hook.t` struct and calls the list of `Akd.Operation.t`
+  corresponding to `rollback` type.
 
-    @doc """
-    Takes a `Akd.Hook.t` struct and calls the list of `Akd.Operation.t`
-    corresponding to `#{ops_type}` type.
+  ## Examples:
 
-    ## Examples:
-
-        iex> hook = %Akd.Hook{}
-        iex> Akd.Hook.#{ops_type}(hook)
-        []
-    """
-    @spec unquote(ops_type)(__MODULE__.t) :: list()
-    def unquote(ops_type)(%__MODULE__{} = hook) do
-      hook
-      |> Map.get(unquote(ops_type))
-      |> Enum.reduce_while([], &runop/2)
-      |> Enum.reverse()
-    end
+      iex> hook = %Akd.Hook{}
+      iex> Akd.Hook.rollback(hook)
+      []
+  """
+  @spec rollback(__MODULE__.t) :: list()
+  def rollback(%__MODULE__{} = hook) do
+    hook
+    |> Map.get(:rollback)
+    |> Enum.reduce_while([], &runop/2)
+    |> Enum.reverse()
   end
 
+
+  @doc """
+  Takes a `Akd.Hook.t` struct and calls the list of `Akd.Operation.t`
+  corresponding to `main` type.
+
+  ## Examples:
+
+      iex> hook = %Akd.Hook{}
+      iex> Akd.Hook.main(hook)
+      []
+  """
+  @spec main(__MODULE__.t) :: list()
+  def main(%__MODULE__{} = hook) do
+    hook
+    |> Map.get(:main)
+    |> Enum.reduce_while([], &runop/2)
+    |> Enum.reverse()
+  end
+
+
+  @doc """
+  Takes a `Akd.Hook.t` struct and calls the list of `Akd.Operation.t`
+  corresponding to `ensure` type.
+
+  If `run_ensure` is `false`, it doesn't run any operations.
+
+  ## Examples:
+
+      iex> hook = %Akd.Hook{}
+      iex> Akd.Hook.ensure(hook)
+      []
+
+      iex> ensure = [%Akd.Operation{dest: %Akd.Destination{}, cmd: "echo 1"}]
+      iex> hook = %Akd.Hook{run_ensure: false, ensure: ensure}
+      iex> Akd.Hook.ensure(hook)
+      []
+  """
+  @spec ensure(__MODULE__.t) :: list()
+  def ensure(%__MODULE__{run_ensure: false}), do: []
+  def ensure(%__MODULE__{} = hook) do
+    hook
+    |> Map.get(:ensure)
+    |> Enum.reduce_while([], &runop/2)
+    |> Enum.reverse()
+  end
+
+  # Delegates to running the operation and translates the return tuple to
+  # :halt vs :cont form usable by `reduce_while`
   defp runop(%Operation{} = op, io) do
     case Operation.run(op) do
       {:error, error} -> {:halt, [error | io]}
