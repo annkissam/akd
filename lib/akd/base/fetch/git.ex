22984eceff5838456adf7805cd88c5d41492c388
Adi <aditya7iyengar@gmail.com>
2017-12-29 12:14:00 -0500

    Update git fetch hook module:
    - Add module documentation
    - Add default opts
    - Add uniq_merge function

--- lib/akd/base/fetch/git.ex
+++ lib/akd/base/fetch/git.ex
@@ -1,15 +1,33 @@
 defmodule Akd.Fetch.Git do
   @moduledoc """
-  TODO: Improve Docs
+  A native Hook module that comes shipped with Akd.
 
-  Fetches source code using Git. This is a basic fetcher that just copies
-  the code that the task is ran from to the build env
+  This module uses `Akd.Hook`.
+
+  Provides a set of operations that fetch source code using `git` to a destination,
+  check out a given branch (defaults to `master`) and pull the latest version
+  of the branch on the destination.
+
+  Ensures to cleanup and empty the desination directory. (Doesn't run this by
+  default)
+
+  Doesn't have any Rollback operations.
+
+  # Defaults:
+
+  * `run_ensure`: `false`
+  * `ignore_failure`: `false`
+  * `branch`: `master`
+
   """
 
   use Akd.Hook
 
+  @default_opts [run_ensure: false, ignore_failure: false, branch: "master"]
+
   def get_hooks(deployment, opts \\ []) do
-    branch = Keyword.get(opts, :branch, "master")
+    opts = uniq_merge(opts, @default_opts)
+    branch = Keyword.get(opts, :branch)
     src = Keyword.get(opts, :src)
     destination = Akd.DestinationResolver.resolve(:build, deployment)
 
@@ -19,11 +37,18 @@ defmodule Akd.Fetch.Git do
   defp fetch_hook(src, branch, destination, opts) do
     form_hook opts do
       main "git clone #{src} .", destination
+      main "git fetch", destination
       main "git checkout #{branch}", destination
       main "git pull", destination
 
       ensure "rm -rf ./*", destination
       ensure "rm -rf ./.*", destination
     end
+  end
+
+  defp uniq_merge(keyword1, keyword2) do
+    keyword1
+    |> Keyword.merge(keyword2)
+    |> Keyword.new()
   end
 end
