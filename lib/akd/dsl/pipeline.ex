08b5dcee92beccc0ad38d70011b4eee8d60ef7f0
Adi <aditya7iyengar@gmail.com>
2017-12-29 15:22:55 -0500

    Update pipeline DSL:
    - Add docs
    - Add moduledoc
    - Add doctests
    - Add comments
    - Cleanup

--- lib/akd/dsl/pipeline.ex
+++ lib/akd/dsl/pipeline.ex
@@ -5,7 +5,7 @@ defmodule Akd.Dsl.Pipeline do
   This modules provides a DSL to interact with Akd in a readable and simple
   manner.
 
-  The router provides a set of macros for generating hooks that could either
+  The module provides a set of macros for generating hooks that could either
   be dispatched to a hook module (native or custom created) or a set of
   operations.
 
@@ -47,6 +47,37 @@ defmodule Akd.Dsl.Pipeline do
   Please refer to `Nomenclature` for more information about the terms used.
   """
 
+  @doc """
+  Defines a pipeline `name` with a given block.
+  Also defines a function with name `name` and arity 0.
+
+  This can be called only inside a module.
+
+  ## Examples:
+
+    iex> defmodule SomeMod do
+    ...>   import Akd.Dsl.Pipeline
+    ...>   pipeline :temporary do
+    ...>     hook "this hook"
+    ...>   end
+    ...> end
+    iex> SomeMod.temporary
+    [{"this hook", []}]
+
+
+    iex> defmodule SomeOtherMod do
+    ...>   import Akd.Dsl.Pipeline
+    ...>   pipeline :temporary do
+    ...>     hook "this hook"
+    ...>   end
+    ...>   pipeline :permanent do
+    ...>    pipe_through :temporary
+    ...>    hook "another hook", some_option: "some option"
+    ...>   end
+    ...> end
+    iex> SomeOtherMod.permanent
+    [{"this hook", []}, {"another hook", [some_option: "some option"]}]
+  """
   defmacro pipeline(name, do: block) do
     quote do
       def unquote(name)() do
@@ -59,6 +90,18 @@ defmodule Akd.Dsl.Pipeline do
     end
   end
 
+  @doc """
+  Adds a hook to a pipeline.
+
+  This can be called only inside a pipeline call.
+
+  ## Examples:
+    pipeline :pipe do
+      hook Akd.Init.Distillery, run_ensure: false
+      hook Akd.Build.Distillery
+      hook Akd.Publish.Distillery
+    end
+  """
   defmacro hook(hook, opts \\ []) do
     quote do
       put_pipe(var!(hooks, unquote(__MODULE__)),
@@ -66,6 +109,24 @@ defmodule Akd.Dsl.Pipeline do
     end
   end
 
+
+  @doc """
+  Adds a list of hooks to a pipeline. Those list of hooks are
+  defined in the pipeline this pipes through
+
+  This can be called only inside a pipeline call.
+
+  ## Examples:
+    pipeline :pipe do
+      hook Akd.Init.Distillery, run_ensure: false
+      hook Akd.Build.Distillery
+      hook Akd.Publish.Distillery
+    end
+
+    pipeline :final do
+      pipe_through :pipe # This adds all the above three hooks to :final
+    end
+  """
   defmacro pipe_through(pipeline) do
     quote do
       __MODULE__
@@ -74,15 +135,25 @@ defmodule Akd.Dsl.Pipeline do
     end
   end
 
-  @doc false
+  @doc """
+  This starts an Agent that keeps track of a pipeline's definition and
+  hooks added to the pipeline.
+  """
   def start_pipe(hooks \\ []), do: Agent.start_link(fn -> hooks end)
 
-  @doc false
+  @doc """
+  This stops the Agent that keeps track of a pipeline's definition and
+  hooks added to the pipeline.
+  """
   def stop_pipe(hooks), do: Agent.stop(hooks)
 
-  @doc false
+  @doc """
+  This adds another hook to the Agent keeping track of a pipeline's definition
+  """
   def put_pipe(hooks, hook), do: Agent.update(hooks, &[hook | &1])
 
-  @doc false
+  @doc """
+  This gets the hooks from the Agent keeping track of a pipeline's definition
+  """
   def get_pipe(hooks), do: hooks |> Agent.get(& &1) |> Enum.reverse()
 end
