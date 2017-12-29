2b0335c1824f9d6770f9d010c1e8b0703cbea567
Adi <aditya7iyengar@gmail.com>
2017-12-29 18:51:45 -0500

    Update Generators:
    - Add Documentation
    - Add module docs
    - Add doc tests
    - Cleanup

--- lib/akd/generator/hook.ex
+++ lib/akd/generator/hook.ex
@@ -1,11 +1,46 @@
 defmodule Akd.Generator.Hook do
-  @moduledoc false
+  @moduledoc """
+  This module handles the generation of custom hooks which use `Akd.Hook`.
+  This can either directly be called, or called through a mix task,
+  `mix akd.gen.hook`.
+
+  This class uses EEx and Mix.Generator to fetch file contents from an eex
+  template and populate the interpolated fields, writing it to the speficied
+  file.
+
+  ## Usage:
+
+  The following call creates a file `hook.ex` at location `path/to/file/hook.ex`
+
+  ```
+  Akd.Generator.Hook.gen(["hook.ex"], path: "path/to/file")
+  ```
+
+  """
 
   require EEx
   require Mix.Generator
 
   @path "lib/"
 
+  @doc """
+  This is the callback implementation for `gen/2`.
+
+  This function takes in a list of inputs and a list of options and generates
+  a module that uses `Akd.Hook` at the specified path with the specified name.
+
+  The first element of the input is expected to be the name of the file.
+
+  The path can be sent to the `opts`.
+
+  If no path is sent, it defaults to #{@path}
+
+  ## Examples:
+
+    Akd.Generator.Hook.gen(["hook.ex"], [path: "some/path"])
+
+  """
+  @spec gen(list, Keyword.t) :: :ok | {:error, String.t}
   def gen([name | _], opts) do
     name
     |> validate_and_format_opts(opts)
@@ -13,21 +48,30 @@ defmodule Akd.Generator.Hook do
     |> write_to_file(name)
   end
 
+  # This function validates the name and options sent to the generator
+  # and formats the options making it ready for the template to read from.
   defp validate_and_format_opts(name, opts) do
     opts = Keyword.put_new(opts, :path, @path)
     [{:name, resolve_name(name)} | opts]
   end
 
+  # This function gets the name of file from the module name
   defp resolve_name(name) do
     Macro.camelize(name)
   end
 
+  # This function gives the location for the template which will be used
+  # by the generator
   defp template(), do:  "#{__DIR__}/templates/hook.ex.eex"
 
+  # This function takes formatted options and returns a tuple.
+  # First element of the tuple is the path to file and second element is
+  # the evaluated file string.
   defp text_from_template(opts) do
     {Keyword.get(opts, :path), EEx.eval_file(template(), assigns: opts)}
   end
 
+  # This function writes contents to a file at a specific path
   defp write_to_file({path, code}, name) do
     path = path <> Macro.underscore(name) <> ".ex"
 
--- lib/akd/generator/task.ex
+++ lib/akd/generator/task.ex
@@ -1,6 +1,20 @@
 defmodule Akd.Generator.Task do
   @moduledoc """
+  This module handles the generation of a custom task which use `Akd.Task`.
+  This can either directly be called, or called through a mix task,
+  `mix akd.gen.task`.
 
+  This class uses EEx and Mix.Generator to fetch file contents from an eex
+  template and populate the interpolated fields, writing it to the speficied
+  file.
+
+  ## Usage:
+
+  The following call creates a file `run.ex` at location `path/to/file/run.ex`
+
+  ```
+  Akd.Generator.Task.gen(["run.ex"], path: "path/to/file")
+  ```
   """
 
   require EEx
@@ -8,8 +22,26 @@ defmodule Akd.Generator.Task do
 
   @path "lib/"
 
+  # Native hook types that can be added using this genenrator
   @hooks ~w(fetch init build publish start stop)a
 
+  @doc """
+  This is the callback implementation for `gen/2`.
+
+  This function takes in a list of inputs and a list of options and generates
+  a module that uses `Akd.Task` at the specified path with the specified name.
+
+  The first element of the input is expected to be the name of the file.
+
+  The path can be sent to the `opts`.
+
+  If no path is sent, it defaults to #{@path}
+
+  ## Examples:
+
+    Akd.Generator.Hook.gen(["task.ex"], [path: "some/path"])
+
+  """
   @spec gen(list, Keyword.t) :: :ok | {:error, String.t}
   def gen([name | _], opts) do
     name
@@ -18,6 +50,8 @@ defmodule Akd.Generator.Task do
     |> write_to_file(name)
   end
 
+  # This function validates the name and options sent to the generator
+  # and formats the options making it ready for the template to read from.
   defp validate_and_format_opts(name, opts) do
     # TODO Do something with opts[:phx]
     opts = @hooks
@@ -27,26 +61,38 @@ defmodule Akd.Generator.Task do
     [{:name, resolve_name(name)} | opts]
   end
 
+  # This function adds the default_hook to a keyword, if the keyword
+  # doesn't have key corresponding to the `hook`. Else just returns the keyword
+  # itself.
   defp resolve_hook_opts(hook, opts) do
     Keyword.put_new(opts, hook, default_string(hook))
   end
 
+  # This function gets default_hook from `Akd` module based on hook type
+  # and converts the module name to string
   defp default_string(hook) do
     Akd
     |> apply(hook, [])
     |> Macro.to_string()
   end
 
+  # This function gets the name of file from the module name
   defp resolve_name(name) do
     Macro.camelize(name)
   end
 
+  # This function gives the location for the template which will be used
+  # by the generator
   defp template(), do:  "#{__DIR__}/templates/task.ex.eex"
 
+  # This function takes formatted options and returns a tuple.
+  # First element of the tuple is the path to file and second element is
+  # the evaluated file string.
   defp text_from_template(opts) do
     {Keyword.get(opts, :path), EEx.eval_file(template(), assigns: opts)}
   end
 
+  # This function writes contents to a file at a specific path
   defp write_to_file({path, code}, name) do
     path = path <> Macro.underscore(name) <> ".ex"
 
