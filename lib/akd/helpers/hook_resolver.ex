104bef4ac2362850e1d9aad79ea27e84b1883607
Adi <aditya7iyengar@gmail.com>
2017-12-29 21:36:18 -0500

    Update Hook Resolver:
    - Add docs
    - Add moduledoc
    - Add doctest
    - Cleanup
    
    - Add test module

--- lib/akd/helpers/hook_resolver.ex
+++ lib/akd/helpers/hook_resolver.ex
@@ -1,22 +1,45 @@
 defmodule Akd.HookResolver do
   @moduledoc """
+  This module defines functions which can be used to resolve hooks, given
+  a `hook_type` and defaults.
+
+  This module is mainly intended to be used by `Akd.DeployHelper` for resolving
+  hooks that it gets from `Akd.Pipeline` DSL.
   """
 
-  @stages ~w(fetch init build publish stop start)a
+  @hook_types ~w(fetch init build publish stop start)a
 
-  for stage <- @stages do
-    binding = stage
+  for hook_type <- @hook_types do
+    binding = hook_type
       |> to_string()
       |> Macro.camelize()
       |> (&Module.concat(Akd, &1)).()
 
-    def unquote(stage)(deployment, []) do
-      unquote(stage)
-      |> (&apply(Akd.Config, &1, [])).()
+    @doc """
+    Returns hooks associated with `#{hook_type}`.
+
+    Takes in a `deployment` and `opts`. If `opts` is empty, just calls
+    the default module.
+
+    ## Examples
+    When `opts` is empty:
+
+      iex> deployment = %Akd.Deployment{mix_env: "prod",
+      ...> build_at: Akd.Destination.local("."),
+      ...> publish_to: Akd.Destination.local("."),
+      ...> name: "name",
+      ...> vsn: "0.1.1"}
+      iex> hooks = Akd.#{hook_type}.get_hooks(deployment, [])
+      iex> Akd.HookResolver.#{hook_type}(deployment, []) == hooks
+      true
+    """
+    def unquote(hook_type)(deployment, []) do
+      unquote(hook_type)
+      |> (&apply(Akd, &1, [])).()
       |> apply(:get_hooks, [deployment, []])
     end
 
-    def unquote(stage)(deployment, opts) do
+    def unquote(hook_type)(deployment, opts) do
       mod = opts[:type]
         |> to_string()
         |> Macro.camelize()
---
+++ test/akd/helpers/hook_resolver_test.exs
@@ -0,0 +1,4 @@
+defmodule Akd.HookResolverTest do
+  use ExUnit.Case
+  doctest Akd.HookResolver
+end
